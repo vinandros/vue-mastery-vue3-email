aaa5ccf89c9f74f41119dc39445eac6f1f5b9a96
Kevin AV <vinandros@gmail.com>
2022-03-12 14:25:23 -0600

    JSON server and MailView, MainTable

---
+++ src/components/MailView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="email-display">
+    <h2 class="mb-0">Subject: <strong>{{email.subject}}</strong></h2>
+    <div><em>From {{email.from}} on {{format(new Date(email.sentAt),"MMM do yyyy")}}</em></div>
+    <div v-html="marked(email.body)"/>
+  </div>
+</template>
+
+<script>
+import { format } from 'date-fns'
+import marked from 'marked'
+export default {
+  name:"MailView",
+  props:{
+    email: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    console.log(props.email)
+    return {
+      format,
+      marked
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
